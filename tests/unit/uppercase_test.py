33d713a84f90a712c5bcde488cac3bc8838e2d48
balutbomber <balutbomber@gmail.com>
2024-03-06 09:55:44 -0800

    updated uppercase_test to assert on conditional else portion

--- tests/unit/uppercase_test.py
+++ tests/unit/uppercase_test.py
@@ -1,9 +1,19 @@
 from ahd_data_pipelines.uppercase import uppercase
 
 # comment on this test so people understand what you are doing.
-def test_correct_return_value_is_given():
+def test_correct_return_value_is_given_when_jeff_is_the_value():
     # setup
     str = "jeff"
+
+    # execute
+    return_value = uppercase(str)
+
+    # validate
+    assert return_value == str.upper()
+
+def test_correct_return_value_is_given_when_something_other_than_jeff_is_given():
+    # setup
+    str = "richard"
 
     # execute
     return_value = uppercase(str)
