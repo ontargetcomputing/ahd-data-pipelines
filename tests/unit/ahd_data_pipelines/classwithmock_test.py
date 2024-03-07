80e054e76d57ed2aa89e1a993cd87cc421a6409f
Jeff Ranta <Jeff.Ranta@CHHS.CA.GOV>
2024-03-07 15:43:03 -0800

    classwithmock_test works now

--- tests/unit/ahd_data_pipelines/classwithmock_test.py
+++ tests/unit/ahd_data_pipelines/classwithmock_test.py
@@ -1,17 +1,19 @@
-from ahd_data_pipelines.classwithmock import uppercasethename
+#from ahd_data_pipelines.classwithmock import uppercasethename
+import ahd_data_pipelines.classwithmock
 from unittest.mock import patch
-import ahd_data_pipelines.uppercase
+#import ahd_data_pipelines.uppercase
 
-
-
-# comment on this test so people understand what you are doing.
 def test_with_mock():
-    with patch('ahd_data_pipelines.uppercase') as mock_uppercase:
-        # Mocking the return value of requests.get
-        mock_uppercase.return_value = "JEFF"
+    with patch('ahd_data_pipelines.classwithmock.uppercasethename') as mock_uppercasethename:
+        # Mocking the return value of 
+        mock_uppercasethename.return_value = "JEFFX"
 
         # Calling the function under test
-        result = uppercasethename("jeff")
+        print(ahd_data_pipelines.classwithmock.uppercasethename("jeff1"))
+        print(ahd_data_pipelines.classwithmock.uppercasethename("jeff2"))
+        print(ahd_data_pipelines.classwithmock.uppercasethename("jeff3"))
+        print(mock_uppercasethename.return_value)
+        result = ahd_data_pipelines.classwithmock.uppercasethename("ccc")
 
         # Asserting that the function behaves as expected
-        assert result == "JEFF"
+        assert result == "JEFFX"
