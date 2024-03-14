64ec1e6a10627603ec919b14795a7cf250c42133
Jeff Ranta <Jeff.Ranta@CHHS.CA.GOV>
2024-03-14 15:10:43 -0700

    updated datasource_factory_test.py

--- tests/unit/ahd_data_pipelines/integrations/datasource_factory_test.py
+++ tests/unit/ahd_data_pipelines/integrations/datasource_factory_test.py
@@ -1,17 +1,36 @@
-
-from ahd_data_pipelines.integrations.datasource_factory import DatasourceFactory
-from ahd_data_pipelines.integrations.db_datasource import DatabricksDatasource
+import unittest
+from unittest.mock import Mock
+from ahd_data_pipelines.integrations.datasource_factory import DatasourceFactory, DatasourceType
 from ahd_data_pipelines.integrations.agol_datasource import AgolDatasource
-from ahd_data_pipelines.integrations.datasource_type import DatasourceType
+from ahd_data_pipelines.integrations.s3_datasource import S3Datasource
+from ahd_data_pipelines.integrations.db_datasource import DatabricksDatasource
+from pyspark.sql import SparkSession
 
-def test_default_datasource_is_databricks():
-    assert type(DatasourceFactory.getDatasource()) is DatabricksDatasource
+class TestDatasourceFactory(unittest.TestCase):
+    
+    def setUp(self):
+        self.mock_dbutils = Mock()
 
-def test_databricks_returned_when_constructed():
-    assert type(DatasourceFactory.getDatasource(type=DatasourceType.DATABRICKS)) is DatabricksDatasource
+    # def test_get_datasource_agol(self):
+    #     self.mock_spark = Mock(spec=SparkSession)
+    #     self.mock_spark.return_value.__len__ = Mock(return_value=1)
+    #     params = {'url': 'https://example.com/agol', 'param1': 'value1'}
+    #     datasource = DatasourceFactory.getDatasource(type=DatasourceType.AGOL, params=params, dbutils=self.mock_dbutils, spark=self.mock_spark)
+    #     self.assertIsInstance(datasource, AgolDatasource)
+    
+    def test_get_datasource_s3(self):
+        self.mock_spark = Mock(spec=SparkSession)
+        self.mock_spark.return_value.__len__ = Mock(return_value=1)
+        params = {'param1': 'value1'}
+        datasource = DatasourceFactory.getDatasource(type=DatasourceType.S3, params=params, dbutils=self.mock_dbutils, spark=self.mock_spark)
+        self.assertIsInstance(datasource, S3Datasource)
+    
+    def test_get_datasource_databricks(self):
+        self.mock_spark = Mock(spec=SparkSession)
+        self.mock_spark.return_value.__len__ = Mock(return_value=1)
+        params = {'param1': 'value1'}
+        datasource = DatasourceFactory.getDatasource(type=DatasourceType.DATABRICKS, params=params, dbutils=self.mock_dbutils, spark=self.mock_spark)
+        self.assertIsInstance(datasource, DatabricksDatasource)
 
-def test_agol_returned_when_constructed():
-    params = {
-      "url": "https://chhsagency.maps.arcgis.com/home/"
-    }
-    assert type(DatasourceFactory.getDatasource(type=DatasourceType.AGOL, params=params)) is AgolDatasource
+if __name__ == '__main__':
+    unittest.main()
