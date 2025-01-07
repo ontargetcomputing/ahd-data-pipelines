787d79bfca5fccd06f551680503b725b99c21b09
balutbomber <balutbomber@gmail.com>
2025-01-07 13:32:44 -0800

    update the install_libkrb5.sh file

--- scripts/install_libkrb5.sh
+++ scripts/install_libkrb5.sh
@@ -3,3 +3,7 @@
 # Run apt-get update and install libkrb5-dev package
 sudo apt-get update
 sudo apt-get install -y libkrb5-dev
+
+# Upgrade pip and install/upgrade Cython
+pip install --upgrade pip
+pip install --upgrade cython
